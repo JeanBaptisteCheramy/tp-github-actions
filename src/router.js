1c0f7a54a9f3a3b2036c3976f6a0f591805351e2
JeanBaptisteCheramy <jeanbaptiste.cheramy@gmail.com>
2026-03-11 14:29:29 +0100

    création api

---
+++ src/router.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('/health', (req, res) => {
+    res.json({status:'ok'})
+})
+
+router.get('/hello/:name', (req, res) => {
+    const {name} = req.params
+    if (!name?.trim()){
+        return res.status(400)?.json({error: 'Name is required'})
+    }
+    res.json({message: `Hello, ${name}!`})
+})
+
+
+module.exports = router
